f1f14d6583d5ab654c1d95c7c5f28753e38157db
PenguinWizard <155591091+PenguinWizard@users.noreply.github.com>
2025-03-08 10:12:23 -0800

    Stuff for Kye to keep working on

---
+++ scripts.js
@@ -0,0 +1,7 @@
+/*
+
+give a creditability percentage but only the percentage, and only 1 number followed by a percent sign. then, Give me 5 pointers in a list that shows why you chose this percent. the list should be this format: title (new line) reasons. for absolutely no reason should you include any other info. only return the info that I input. keep all the responses concise and accurate. 
+
+here is the article: 
+
+*/
